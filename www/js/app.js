16ba794199ea8f2a87db59907f2a933093abafa3
Steven <stevenyong5303@msn.com>
2018-11-08 12:20:04 +0800

    1.0.30
    Test on plugin.

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ var app = new Framework7({
 			  id: 'com.wkv.manage',
 			  name: 'WKV',
 			  theme: 'md',
-			  version: "1.0.29",
+			  version: "1.0.30",
 			  rtl: false,
 			  language: "en-US"
 		  });
