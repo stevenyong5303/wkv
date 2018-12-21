2afb09250d52d6aeee408a6204bcb3eaad4c3e9d
Steven <stevenyong5303@msn.com>
2018-12-21 18:27:12 +0800

    1.0.33
    Splashscreen removed.

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ var app = new Framework7({
 			  id: 'com.wkv.manage',
 			  name: 'WKV',
 			  theme: 'md',
-			  version: "1.0.32",
+			  version: "1.0.33",
 			  rtl: false,
 			  language: "en-US"
 		  });
