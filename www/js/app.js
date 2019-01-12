9b4e896c9e1cae548179b2292cbccec7184f57ef
Steven <stevenyong5303@msn.com>
2019-01-12 21:26:52 +0800

    1.0.59
    1. Fixed on config.xml, change minimum sdk verson to 19.

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@ var app = new Framework7({
 			  id: 'com.wkv.manage',
 			  name: 'WKV',
 			  theme: 'md',
-			  version: "1.0.58",
+			  version: "1.0.59",
 			  rtl: false,
 			  language: "en-US"
 		  });
