2c616e007ca98a79ec31fb5cf400767df8e19a2d
Steven <stevenyong5303@msn.com>
2019-10-19 21:00:18 +0800

    1.0.188
    1. Continue testing on notification feedback.

--- www/js/app.js
+++ www/js/app.js
@@ -6,11 +6,11 @@ var apps = new Framework7({
 			  id: 'com.wkv.manage',
 			  name: 'WKV',
 			  theme: 'md',
-			  version: "1.0.187",
+			  version: "1.0.188",
 			  rtl: false,
 			  language: "en-US"
 		  });
-var geoToken = true, geoCount = 120, APP_VERSION = 10187, tmpCalendar = '';
+var geoToken = true, geoCount = 120, APP_VERSION = 10188, tmpCalendar = '';
 
 var app = {
     initialize: function() {
@@ -26,7 +26,7 @@ var app = {
 		
 		var notificationOpenedCallback = function(jsonData) {
 			console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-			apps.dialog.alert(JSON.stringify(jsonData['notification']));
+			apps.dialog.alert(JSON.stringify(jsonData['notification']['payload']['additionalData']));
 		};
 
 		window.plugins.OneSignal
