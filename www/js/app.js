1ff360d78e0f3edb792ff4285743a1037c49de3f
Steven <stevenyong5303@msn.com>
2020-02-09 03:27:46 +0800

    1.0.241
    1. Bug fix on deliver app to ios.

--- www/js/app.js
+++ www/js/app.js
@@ -6,11 +6,11 @@ var apps = new Framework7({
 			  id: 'com.wkv.manage',
 			  name: 'WKV',
 			  theme: 'md',
-			  version: "1.0.240",
+			  version: "1.0.241",
 			  rtl: false,
 			  language: "en-US"
 		  });
-var geoToken = true, geoCount = 60, APP_VERSION = 10240, tmpCalendar = '', fileObject, tapHold = 0, tapHoldStr = '';
+var geoToken = true, geoCount = 60, APP_VERSION = 10241, tmpCalendar = '', fileObject, tapHold = 0, tapHoldStr = '';
 
 var app = {
     initialize: function() {
